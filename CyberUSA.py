e698f2fa4ec34ae60ddaa5348cdabdbdb2e0ab47
CyberUSA <60077355+CyberUSA@users.noreply.github.com>
2020-01-28 16:37:00 +0300

    Add files via upload

---
+++ CyberUSA.py
@@ -0,0 +1,45 @@
+import requests
+import html2text
+import bs4
+import os
+banner = 'Разработчик: CyberUSA\nVK: https://vk.com/CyberUSA\nТелеграм: @CyberUSA\n\n'
+
+print(banner)
+print('CYBER DEANONE V 1.1')
+
+def avito():
+	print('Парсер данных с авито\n')
+	phone = input('Введите номер: +7')
+	res = requests.get('https://mirror.bullshit.agency/search_by_phone/' + phone)
+	b=bs4.BeautifulSoup(res.text, "html.parser")
+
+	p = b.select('.text-muted')
+	k = 0
+	for i in p:
+		ps = p[k].getText()
+		print(ps.strip())
+		k=k+1
+
+##avito(phone)
+##input('Нажмите что бы продолжить')
+
+
+def operator():
+	
+	phone = input('Введите номер: +7')
+	res = requests.get('https://tel-search.ru/numbers/phone=' + phone)
+	b=bs4.BeautifulSoup(res.text, "html.parser")
+	p = b.select('.jumbotron')
+	k = 0
+	os.system('clear')
+	print('Данные по номеру\n\n')
+	for i in p:
+		ps = p[k].getText()
+		print(ps.strip(), '\n\n')
+		k=k+1
+		
+		
+operator()
+input('\nНажмите ENTER\n\n')
+os.system('clear')
+avito()
