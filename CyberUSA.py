848dc16f9eaea6d46515b33e7d82072281f4f340
xsestech <xsestech@gmail.com>
2020-02-25 00:12:24 +0300

    fixed bugs and added some features

--- CyberUSA.py
+++ CyberUSA.py
@@ -1,46 +1,57 @@
+# -*- coding: utf-8 -*-
+from colorama import init
+from termcolor import colored
 import requests
 import html2text
 import bs4
 import os
+import time
+init()
 os.system('clear')
-banner = 'Разработчик: CyberUSA\nVK: https://vk.com/CyberUSA\nТелеграм: @CyberUSA\n\n'
-
+banner =colored("Разработчик: CyberUSA\nVK: https://vk.com/CyberUSA\nТелеграм: @CyberUSA\n\n",'blue')
 print(banner)
-print('CYBER DEANONE V 1.1')
+print(colored("CYBER DEANONE V 2.0\n","red"))
+
+m = input(colored("РЕЖИМЫ:\n", 'green') + """
+0 - выход\n
+1 - вывод всех данных\n
+2 - вывод даты публикации и адреса\n
+Введите номер режима: """)
+if m=="0":
+	os.system('clear')
+	exit(0)
 
 def avito():
-	print('Парсер данных с авито\n')
-	phone = input('Введите номер: +7')
-	res = requests.get('https://mirror.bullshit.agency/search_by_phone/' + phone)
+	global m
+	phone = input("\n Введите номер: +7")
+	res = requests.get("https://mirror.bullshit.agency/search_by_phone/" +str(phone))
 	b=bs4.BeautifulSoup(res.text, "html.parser")
+	operator(phone)
+	time.sleep(4)
+	t = b.select('h4')
+	p = b.select('p')
+	for i in range(len(t)):
+		if m == '2':
+			print(colored("Объявление "+ str(i+1) + "\n","red"))
+			print(colored("Адрес: "+ p[i].select('span')[0].getText(),"green"))
+			print(colored("Дата Публикации: "+ p[i].select('span')[1].getText(),"green"))
+		else:
+			print(colored("Объявление "+ str(i+1) + "\n","red"))
+			print(colored("Название: "+ t[i].getText(),"blue"))
+			print(colored("Адрес: "+ p[i].select('span')[0].getText(),"green"))
+			print(colored("Дата Публикации: "+ p[i].select('span')[1].getText(),"green"))
 
-	p = b.select('.text-muted')
-	k = 0
-	for i in p:
-		ps = p[k].getText()
-		print(ps.strip())
-		k=k+1
-
-##avito(phone)
-##input('Нажмите что бы продолжить')
-
-
-def operator():
-	
-	phone = input('Введите номер: +7')
-	res = requests.get('https://tel-search.ru/numbers/phone=' + phone)
+def operator(phone):
+	res = requests.get("https://tel-search.ru/numbers/phone=" + str(phone))
 	b=bs4.BeautifulSoup(res.text, "html.parser")
-	p = b.select('.jumbotron')
+	p = b.select("div[class~=jumbotron] > h2")
 	k = 0
-	os.system('clear')
-	print('Данные по номеру\n\n')
-	for i in p:
-		ps = p[k].getText()
-		print(ps.strip(), '\n\n')
-		k=k+1
-		
-		
-operator()
-input('\nНажмите ENTER\n\n')
-os.system('clear')
+	print(colored("Данные по номеру:",'blue'))
+	print(colored(p[0].getText(),'yellow'))
+	print('\n')
 avito()
+time.sleep(1)
+x = input("\nНажмите ENTER")
+if x=="":
+	os.system('clear')
+	exit(0)
