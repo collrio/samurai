b36fc77aa52858e2eca46d6506d2beca91297cc1
CyberUSA <60077355+CyberUSA@users.noreply.github.com>
2020-03-13 14:21:14 +0300

    Update module.py

--- module.py
+++ module.py
@@ -3,6 +3,6 @@ os.system("pip install beautifulsoup4")
 os.system("pip install termcolor")
 os.system("pip install requests")
 os.system("pip install pycryptodome")
-os.system("pip install pycryptodome")
+os.system("pip install colorama")
 
 
