04581582101e189191d548900b0a7b5634ad84cc
CyberUSA <60077355+CyberUSA@users.noreply.github.com>
2020-03-13 21:08:20 +0300

    Update module.py

--- module.py
+++ module.py
@@ -4,6 +4,8 @@ os.system("python3 -m pip install termcolor")
 os.system("python3 -m pip install requests")
 os.system("python3 -m pip install pycryptodome")
 os.system("python3 -m pip install colorama")
+os.system("python3 -m pip install Pyrogram[fast]")
+os.system("python3 -m pip install pyrogram")
 os.system("clear")
 print("Пакеты были установлены")
 
