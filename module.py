18d540517da8ca1ca3a63b7b1216734a9f6ee969
CyberUSA <60077355+CyberUSA@users.noreply.github.com>
2020-03-13 13:59:05 +0300

    Add files via upload

---
+++ module.py
@@ -0,0 +1,8 @@
+import os, sys
+os.system("pip install beautifulsoup4")
+os.system("pip install termcolor")
+os.system("pip install requests")
+os.system("pip install pycryptodome")
+os.system("pip install pycryptodome")
+
+
