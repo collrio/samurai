5c0c7c9f3cc4110652b8d4e3ebdcb6c64ddc0686
CyberUSA <60077355+CyberUSA@users.noreply.github.com>
2020-03-13 14:57:46 +0300

    Update module.py

--- module.py
+++ module.py
@@ -1,9 +1,9 @@
 import os, sys
-os.system("python -m pip install beautifulsoup4")
-os.system("python -m pip install termcolor")
-os.system("python -m pip install requests")
-os.system("python -m pip install pycryptodome")
-os.system("python -m pip install colorama")
+os.system("python3 -m pip install beautifulsoup4")
+os.system("python3 -m pip install termcolor")
+os.system("python3 -m pip install requests")
+os.system("python3 -m pip install pycryptodome")
+os.system("python3 -m pip install colorama")
 os.system("clear")
 print("Пакеты были установлены")
 
